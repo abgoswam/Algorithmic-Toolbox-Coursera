621dd14583bb84f704e0b1eac2bb58b5f5512640
Abhishek Goswami <abgoswam@gmail.com>
2018-07-23 15:44:42 -0700

    incomplete pb 6

--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,10 +1,34 @@
 #Uses python3
 
 import sys
-
+import numpy as np
 
 def lcs3(a, b, c):
     #write your code here
+
+    n = len(a)
+    m = len(b)
+    l = len(c)
+
+    lcs = np.zeros((n+1, m+1, l+1), np.int32)
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            for k in range(1, l+1):
+                v = [
+                    lcs[i, j, k-1],
+                    lcs[i, j-1, k],
+                ]
+
+                if a[i-1] == b[j-1] and b[j-1] == c[k-1]:
+                    v.append(lcs[i-1, j-1, k-1] + 1)
+                else:
+                    v.append(lcs[i-1, j-1, k-1])
+
+                lcs[i, j, k] = max(v)
+
+    return lcs[n, m]
+
     return min(len(a), len(b), len(c))
 
 
